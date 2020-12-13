8dbdd0a3af04f29a5b619c9d6c87706671b9e4e9
Dave Spina <dspina79@gmail.com>
2020-12-12 21:18:02 -0500

    Project initialization

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Dave Spina on 12/12/20.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
